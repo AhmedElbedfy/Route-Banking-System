9d7040056f24cc04b970cdf37ea8c5beec9ce7db
Ahmed Khaled <ahmed.khaled.hussain@gmail.com>
2024-04-07 02:13:05 +0200

    Add comment notes

--- Employee.h
+++ Employee.h
@@ -2,27 +2,22 @@
 #include "Person.h"
 #include "Client.h"
 
-
-
 class Employee :
 	public Person
 {
 
 private:
-
 	double salary;
 
 public:
 
 	void login(int id, std::string password)override {}
-
 	void addClient(Client client) {}
-
 	Client searchClient(int id) {}
-
 	void editClientInfo(std::string name, std::string password) {}
 	void editClientInfo(std::string name) {}
-
 	void displayClientInfo() {}
 
+	// Need Work - remove from Admin
+	void listEmployees() {}
 };
